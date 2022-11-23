54af3fc6c447d1e78563bb5b092b68554bf7c627
soumalla <soumallatarafder@gmail.com>
2022-11-23 21:04:06 +0530

    prediction work started1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, Soumalla!'
 
 if __name__ == '__main__':
     app.run(debug=True)
